2392583d827f249fc07c286dd2cd050b0235114c
jacobimus <jacobo.uribe.bedoya@gmail.com>
2025-02-03 20:09:16 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+let timer = 0
+input.onButtonPressed(Button.A, function () {
+    timer = randint(5, 15)
+    basic.showLeds(`
+        . # # # .
+        # # # # #
+        # # # # #
+        # # # # #
+        . # # # .
+        `)
+    while (timer > 0) {
+        timer += -1
+        basic.pause(1000)
+    }
+    basic.showIcon(IconNames.Skull)
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.BaDing), music.PlaybackMode.InBackground)
+})
